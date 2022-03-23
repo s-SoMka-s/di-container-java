1af5ae174f91558324288c925decfdeb185ce04f
i.chiesov <i.chiesov@gmail.com>
2022-03-23 14:21:19 +0700

    Add custom annotations.

---
+++ src/main/java/framework/annotations/Component.java
@@ -0,0 +1,14 @@
+package framework.annotations;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Component {
+    String value() default "";
+}
--- src/main/java/framework/annotations/Scope.java
+++ src/main/java/framework/annotations/Scope.java
@@ -1,10 +1,11 @@
 package framework.annotations;
 
+
 import java.lang.annotation.*;
 
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface Scope {
-    String value();
+    framework.enums.Scope value();
 }
