ed6737369c62ee4b9f791c994b6d28dde77c0cb0
i.chiesov <i.chiesov@gmail.com>
2022-03-23 14:52:09 +0700

    Add simple bean store.

---
+++ src/main/java/framework/beans/BeanStore.java
@@ -0,0 +1,24 @@
+package framework.beans;
+
+import java.util.HashMap;
+
+public class BeanStore {
+    private final HashMap<String, Bean> beans;
+
+    public BeanStore(){
+        this.beans = new HashMap<>();
+    }
+
+    public void add(Bean bean) {
+        var name = bean.getName();
+        beans.put(name, bean);
+    }
+
+    public Bean get(String name) {
+        if (!beans.containsKey(name)) {
+            return null;
+        }
+
+        return beans.get(name);
+    }
+}
